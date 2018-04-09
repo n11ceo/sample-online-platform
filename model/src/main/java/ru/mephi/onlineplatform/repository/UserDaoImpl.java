3f9e26f5e032c793a98d5671016673a26279fe66
Nikolaychik Aleksandr <aleks.nik.w@gmail.com>
2018-04-09 15:21:21 +0300

    made implementation

---
+++ model/src/main/java/ru/mephi/onlineplatform/repository/UserDaoImpl.java
@@ -0,0 +1,26 @@
+package ru.mephi.onlineplatform.repository;
+
+import java.util.List;
+
+public class UserDaoImpl implements UserDao {
+    @Override
+    public User save(User user) {
+        return null;
+    }
+
+    @Override
+    public User edit(Long id, User user) {
+        return null;
+    }
+
+    @Override
+    public void delete(Long id) {
+
+    }
+
+    @Override
+    public List<User> selectByRole(SystemRole systemRole) {
+        return null;
+    }
+}
+    
